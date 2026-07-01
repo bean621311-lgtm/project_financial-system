ba16f48490c7ddd915664b2168f1de233a9a119d
kcsidhant03-source <kcsidhant03@gmail.com>
2026-07-01 14:54:43 +0545

    Update email_validation.py

---
+++ main/services/email_validation.py
@@ -0,0 +1,8 @@
+from email_validator import validate_email, EmailNotValidError
+
+def validate_user_email(email):
+    try:
+        valid = validate_email(email)
+        return True, valid.email
+    except EmailNotValidError as e:
+        return False, str(e)
